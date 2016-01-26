ee608ebaae1ff0693fb7bdb8f57145c9b33d19ee
Chigozie <Chygoz2@gmail.com>
2016-01-26 15:27:11 +0000

    Added the main method to a seperate file

---
+++ TeamProject.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class TeamProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame gui = new TimetableGUI();
+		gui.setVisible(true);
+
+	}
+}
